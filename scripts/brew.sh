ba328d9f25d047a71e3d5079d5bcf3708df8b402
Manuel de la Peña <mdelapenya@gmail.com>
2021-04-10 21:24:45 +0200

    chore: move formulas to  aseparate file

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BASEDIR=$(dirname "$0")
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
@@ -16,12 +18,6 @@ BREW_PREFIX=$(brew --prefix)
 brew install coreutils
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
@@ -32,44 +28,14 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install screen
-brew install gmp
-
-brew install jq
-
-# Install other useful binaries.
-brew install ack
-brew install tree
-
 brew cask install google-cloud-sdk
-brew install maven
-brew install vagrant
 brew cask install virtualbox
+
+while read f; do
+  brew install $f
+done <${BASEDIR}/formulas.txt
+
 vagrant plugin install vagrant-vbguest vagrant-disksize
-
-brew install hub
-brew install pre-commit
-
-brew install spectacle
-brew install visual-studio-code
-brew install intellij-idea-ce
-brew install rescuetime
-brew install kubectl
-brew install brave-browser
-brew install docker
-brew install google-cloud-sdk
-brew install vault
-brew install htop
-brew install homebrew/cask/docker
 
 # Yubikey
 brew install gnupg yubikey-personalization hopenpgp-tools ykman pinentry-mac
