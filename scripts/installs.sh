3797da1747c9af2fa1a521aa02ff94876a666e13
Manuel de la Peña <mdelapenya@gmail.com>
2025-10-13 08:07:27 +0200

    chore: install gvm

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install GVM
+curl -sL -o /usr/local/bin/gvm https://github.com/andrewkroh/gvm/releases/download/v0.5.2/gvm-darwin-all
+chmod +x /usr/local/bin/gvm
+eval "$(gvm 1.24.7)"
+go version
+
 # Install SDKMAN
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
